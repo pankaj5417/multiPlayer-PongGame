be7409c733b72711d4b5936006cc4d694a888876
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-08-18 08:38:55 +0530

    updated the opponent and synchronised the paddle and ball

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ server.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}...`);
 
 });
+let readyPlayerCount=0;
+io.on('connection',(socket)=>{
+    console.log('a user connected',socket.id)
+    socket.on('ready',()=>{
+        console.log("Player ready",socket.id)
+        readyPlayerCount++;
+        if(readyPlayerCount===2){
+            //broadcast
+            io.emit('startGame',socket.id)
+        }
+    })
+    socket.on('paddleMove',(paddleData)=>{
+       socket.broadcast.emit('paddleMove',paddleData) 
+    })
+    socket.on('ballMove',(ballData)=>{
+        socket.broadcast.emit('ballMove',ballData) 
+     })
+})
